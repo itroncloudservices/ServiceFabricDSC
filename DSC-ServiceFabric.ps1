3d63b7be20db3e6165e1b30f94b0911e9618f546
Jeff Bailey <jeff.bailey@itron.com>
2017-05-24 17:39:41 -0700

    Initial

---
+++ DSC-ServiceFabric.ps1
@@ -0,0 +1,63 @@
+﻿Configuration ServiceFabric
+{  
+	  
+    Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
+    
+    $NTPServers = "1.pool.ntp.org,2.pool.ntp.org"
+    
+    Node ServiceFabricNode
+	{
+
+        Script SetServers
+        { 
+            #Credential = $cred
+            GetScript=
+            {
+                $res = w32tm /query /peers
+                $groups = ([regex]::Match($res,'Peer: (\S*)')).Groups
+                if ($groups.Count -eq 2)
+                {
+                    write-warning "GetScript " $groups[1].Value
+                    return @{Result = $groups[1].Value};
+                }
+                else
+                {
+                    write-warning "GetScript - blank"
+                    return @{Result = ""};
+                }
+            }
+            SetScript = 
+            { 
+                $NTPServers = "time.windows.com,0x9"
+        	    write-warning "INFO : Configure NTP Service with param : $NTPServers"
+			    w32tm /configure /manualpeerlist:$NTPServers /syncfromflags:manual /update
+                if ((Get-Service -name W32Time).status -eq "Running" -eq $true)
+                {
+                    write-warning  "INFO : Stop NTP Service"	
+			        net stop w32time 
+                }
+	            write-warning "INFO : Start NTP Service"
+			    net start w32time
+
+            }
+            TestScript=
+            {
+                $NTPServers = "time.windows.com,0x9"
+                $res = w32tm /query /peers
+                $groups = ([regex]::Match($res,'Peer: (\S*)')).Groups
+                if ($groups.Count -eq 2 -And $groups[1].Value -eq $NTPServers)
+                {
+                    return $true;
+                }
+                else
+                {
+                    return $false;
+                }
+            
+            }
+
+        }  
+    }
+
+	
+} 
---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+## Service Fabric DSC
+Desired state configuration scripts to control service fabric nodes.
+
+* Set NTP
