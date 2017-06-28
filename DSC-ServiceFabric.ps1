e068bf562511d4cb2d554b99d6bc2123ad3e44e7
Jeff Bailey <jeff.bailey@itron.com>
2017-06-28 07:41:46 -0700

    Update NTP to partially disable hyper v sync

--- DSC-ServiceFabric.ps1
+++ DSC-ServiceFabric.ps1
@@ -2,12 +2,9 @@
 {  
 	  
     Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
-    
-    $NTPServers = "1.pool.ntp.org,2.pool.ntp.org"
-    
+        
     Node ServiceFabricNode
 	{
-
         Script SetServers
         { 
             #Credential = $cred
@@ -28,24 +25,26 @@
             }
             SetScript = 
             { 
-                $NTPServers = "1.pool.ntp.org,2.pool.ntp.org"
-        	    write-warning "INFO : Configure NTP Service with param : $NTPServers"
-			    w32tm /configure /manualpeerlist:$NTPServers /syncfromflags:manual /update
-                if ((Get-Service -name W32Time).status -eq "Running" -eq $true)
-                {
-                    write-warning  "INFO : Stop NTP Service"	
-			        net stop w32time 
-                }
-	            write-warning "INFO : Start NTP Service"
-			    net start w32time
-
+                write-warning "INFO : Setting NTP info"
+                REG ADD HKLM\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\VMICTimeProvider /v Enabled /t REG_DWORD /d 0 /f 
+                REM Set the service
+                W32TM /config /manualpeerlist:"time.windows.com,0x9" /syncfromflags:MANUAL /update
+                
+                REG ADD HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Config /v MinPollInterval /t REG_DWORD /d 8 /f 
+                REG ADD HKLM\SYSTEM\CurrentControlSet\Services\W32Time\Config /v MaxPollInterval /t REG_DWORD /d 10 /f 
+                
+                REM Windows Time Restart Service
+                NET STOP W32TIME
+                NET START W32TIME
+                w32tm /resync 
             }
             TestScript=
             {
-                $NTPServers = "1.pool.ntp.org,2.pool.ntp.org"
+                $reg = Get-ItemProperty -Path HKLM:SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\VMICTimeProvider -Name Enabled
+                $NTPServers = "time.windows.com,0x9"
                 $res = w32tm /query /peers
                 $groups = ([regex]::Match($res,'Peer: (\S*)')).Groups
-                if ($groups.Count -eq 2 -And $groups[1].Value -eq $NTPServers)
+                if ($groups.Count -eq 2 -And $groups[1].Value -eq $NTPServers -and $reg.Enabled -eq 0 )
                 {
                     return $true;
                 }
@@ -53,11 +52,7 @@
                 {
                     return $false;
                 }
-            
             }
-
         }  
     }
-
-	
 } 
